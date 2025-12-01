dfe667629803f6822f55789464fdd78bf633dfba
Edward Kurita <edwardcskurita@gmail.com>
2025-12-01 00:09:20 -0500

    Fixed github problems by creating a new repo. Project is mostly empty except for ERD.pdf

---
+++ README.md
@@ -0,0 +1,4 @@
+COMP 3005 Final Project
+=
+## Configuring the PostgreSQL Database
+The program looks for a database called "comp_3005_final_project"
---
+++ app/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
+public class Main {
+    public static void main() {
+        String url = "jdbc:postgresql://localhost:5433/comp_3005_final_project";
+        String user = "postgres";
+        String password = "password";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(url, user, password);
+            if (connection != null) {
+                System.out.println("Connected to the database");
+            } else {
+                System.out.println("Failed to connect to the database");
+            }
+
+        } catch (Exception e) {
+        }
+    }
+}
